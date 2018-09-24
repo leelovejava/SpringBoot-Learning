f0bc8654c24222658daeddc08036f311c56c267a
Administrator <1165746141@qq.com>
2018-09-24 14:25:16 +0800

    Spring Hadoop

---
+++ Spring-Hadoop/src/test/java/com/leelovejava/hadoop/SpringBootHDFSApp.java
@@ -0,0 +1,32 @@
+package com.leelovejava.hadoop;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.hadoop.fs.FsShell;
+
+/**
+ * @program: hadoop-train
+ * @description: 使用spring boot来访问HDFS
+ * @author: 01
+ * @create: 2018-04-04 18:45
+ **/
+@SpringBootApplication
+public class SpringBootHDFSApp implements CommandLineRunner {
+
+    @Autowired
+    private FsShell fsShell;  // 用于执行hdfs shell命令的对象
+
+    public void run(String... strings) throws Exception {
+        // 查看根目录下的所有文件
+        for (FileStatus fileStatus : fsShell.ls("/")) {
+            System.out.println("> " + fileStatus.getPath());
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHDFSApp.class, args);
+    }
+}
---
+++ Spring-Hadoop/src/test/java/com/leelovejava/hadoop/SpringHadoopApp.java
@@ -0,0 +1,57 @@
+package com.leelovejava.hadoop;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * @program: hadoop-train
+ * @description: 使用Spring Hadoop来访问HDFS文件系统
+ * @author: tianhao
+ * @create: 2018-04-04 17:39
+ **/
+public class SpringHadoopApp {
+
+    private ApplicationContext ctx;
+    private FileSystem fileSystem;
+
+    @Before
+    public void setUp() {
+        ctx = new ClassPathXmlApplicationContext("beans.xml");
+        fileSystem = (FileSystem) ctx.getBean("fileSystem");
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        ctx = null;
+        fileSystem.close();
+    }
+
+    /**
+     * 在HDFS上创建一个目录
+     * @throws Exception
+     */
+    @Test
+    public void testMkdirs()throws Exception{
+        fileSystem.mkdirs(new Path("/SpringHDFS/"));
+    }
+
+    /**
+     * 读取HDFS上的文件内容
+     * @throws Exception
+     */
+    @Test
+    public void testText()throws Exception{
+        FSDataInputStream in = fileSystem.open(new Path("/SpringHDFS/1.txt"));
+        IOUtils.copyBytes(in, System.out, 1024);
+        in.close();
+    }
+}
