d1093c442835ae7fe52275f020f1515356cd396d
tianhao <1165746141@qq.com>
2018-11-07 21:11:08 +0800

    格式

--- hbase-demo/src/test/java/com/leelovejava/hbase/HBaseConn.java
+++ hbase-demo/src/test/java/com/leelovejava/hbase/HBaseConn.java
@@ -14,55 +14,55 @@ import org.apache.hadoop.hbase.client.Table;
  */
 public class HBaseConn {
 
-  private static final HBaseConn INSTANCE = new HBaseConn();
-  private static Configuration configuration;
-  private static Connection connection;
+    private static final HBaseConn INSTANCE = new HBaseConn();
+    private static Configuration configuration;
+    private static Connection connection;
 
-  private HBaseConn() {
-    try {
-      if (configuration == null) {
-        configuration = HBaseConfiguration.create();
-        configuration.set("hbase.zookeeper.quorum", "192.168.0.110:2181");
-        configuration.set("HADOOP_HOME","C:/Program Files (x86)/hadoop");
-        // HBase RegionServer绑定的端口
-        //configuration.set("hbase.regionserver.port","192.168.109.131:16020");
-        // HBase的Master的端口,默认60000
-        //configuration.set("hbase.master.port","192.168.109.131:60000");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    private HBaseConn() {
+        try {
+            if (configuration == null) {
+                configuration = HBaseConfiguration.create();
+                configuration.set("hbase.zookeeper.quorum", "192.168.0.110:2181");
+                configuration.set("HADOOP_HOME", "C:/Program Files (x86)/hadoop");
+                // HBase RegionServer绑定的端口
+                //configuration.set("hbase.regionserver.port","192.168.109.131:16020");
+                // HBase的Master的端口,默认60000
+                //configuration.set("hbase.master.port","192.168.109.131:60000");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private Connection getConnection() {
-    if (connection == null || connection.isClosed()) {
-      try {
-        connection = ConnectionFactory.createConnection(configuration);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+    private Connection getConnection() {
+        if (connection == null || connection.isClosed()) {
+            try {
+                connection = ConnectionFactory.createConnection(configuration);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
     }
-    return connection;
-  }
 
-  public static Connection getHBaseConn() {
-    return INSTANCE.getConnection();
-  }
+    public static Connection getHBaseConn() {
+        return INSTANCE.getConnection();
+    }
 
-  public static Table getTable(String tableName) throws IOException {
-    return INSTANCE.getConnection().getTable(TableName.valueOf(tableName));
-  }
+    public static Table getTable(String tableName) throws IOException {
+        return INSTANCE.getConnection().getTable(TableName.valueOf(tableName));
+    }
 
     /**
      * 关闭HBaseAdmin实例的所有资源，包括与远程服务器的连接
      */
-  public static void closeConn() {
-    if (connection != null) {
-      try {
-        connection.close();
-      } catch (IOException ioe) {
-        ioe.printStackTrace();
-      }
+    public static void closeConn() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
     }
-  }
 }
