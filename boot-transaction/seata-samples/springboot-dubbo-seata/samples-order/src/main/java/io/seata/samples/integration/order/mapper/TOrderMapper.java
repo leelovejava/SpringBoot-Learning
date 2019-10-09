05ee64be056baed8a908fbf051e2080331b34e32
tianhao <tianhao@caimom.com>
2019-10-09 13:50:14 +0800

    seata

---
+++ boot-transaction/seata-samples/springboot-dubbo-seata/samples-order/src/main/java/io/seata/samples/integration/order/mapper/TOrderMapper.java
@@ -0,0 +1,23 @@
+package io.seata.samples.integration.order.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import io.seata.samples.integration.order.entity.TOrder;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author heshouyou
+ * @since 2019-01-13
+ */
+public interface TOrderMapper extends BaseMapper<TOrder> {
+
+    /**
+     * 创建订单
+     * @Param:  order 订单信息
+     * @Return:
+     */
+    void createOrder(@Param("order") TOrder order);
+}
---
+++ boot-transaction/seata-samples/springcloud-jpa-seata/order-service/src/main/java/io/seata/sample/entity/Order.java
@@ -0,0 +1,67 @@
+package io.seata.sample.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "order_tbl")
+@DynamicUpdate
+@DynamicInsert
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private String commodityCode;
+    private BigDecimal money;
+    private Integer count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCommodityCode() {
+        return commodityCode;
+    }
+
+    public void setCommodityCode(String commodityCode) {
+        this.commodityCode = commodityCode;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
