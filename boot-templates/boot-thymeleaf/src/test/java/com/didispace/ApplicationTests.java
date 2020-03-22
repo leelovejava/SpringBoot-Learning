54570300be17b599258dd72e587f694e944e1c2c
leelovejava <1165746141@qq.com>
2020-03-22 12:21:48 +0800

    fix 测试

--- boot-templates/boot-thymeleaf/src/test/java/com/didispace/ApplicationTests.java
+++ boot-templates/boot-thymeleaf/src/test/java/com/didispace/ApplicationTests.java
@@ -3,12 +3,8 @@ package com.didispace;
 import com.didispace.web.HelloController;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,30 +15,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 /**
- *
  * @author 程序猿DD
  * @version 1.0.0
  * @blog http://blog.didispace.com
- *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
-@WebAppConfiguration
+///@RunWith(SpringJUnit4ClassRunner.class)
+///@SpringApplicationConfiguration(classes = MockServletContext.class)
+///@WebAppConfiguration
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
-	private MockMvc mvc;
+    private MockMvc mvc;
 
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(
-				new HelloController()).build();
-	}
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(
+                new HelloController()).build();
+    }
 
-	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
-	}
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+    }
 
 }
