0d167fc2ffe56292a7a2c1fad3655cd9487401a9
leelovejava <1165746141@qq.com>
2020-03-22 12:11:59 +0800

    添加web依赖

--- boot-templates/boot-thymeleaf/src/main/java/com/didispace/web/HelloController.java
+++ boot-templates/boot-thymeleaf/src/main/java/com/didispace/web/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
