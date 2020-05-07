d1875c5230f0a9eb8b483850029629381b8eaddb
leelovejava <1165746141@qq.com>
2020-05-07 22:34:53 +0800

    添加注释

--- boot-datasource/boot-sqllite/src/main/java/com/leelovejava/boot/sqlite/config/MyBatisMapperScannerConfig.java
+++ boot-datasource/boot-sqllite/src/main/java/com/leelovejava/boot/sqlite/config/MyBatisMapperScannerConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+/**
+ * @author leelovejava
+ */
 @Configuration
 public class MyBatisMapperScannerConfig {
     @Bean
